1d304b8f7242e48d43ca0d2eb91eff3353db0f82
Patchcoat <spiencewelch@gmail.com>
2023-01-14 18:53:39 -0700

    initial commit

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description: The Main file for the Classic Rogelike Graphics API 
+ *
+ *        Version:  1.0
+ *        Created:  01/14/2023 06:41:04 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Spencer Welch
+ *   Organization:  BYU-I
+ *
+ * =====================================================================================
+ */
+#include <raylib.h>
+#include <stdlib.h>
+
+int main() {
+
+    return 1;
+}
