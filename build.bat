e692fe7fd9931d57241d0b533adb7377524af4b0
Patchcoat <spiencewelch@gmail.com>
2023-08-10 19:17:48 -0500

    improved build file and gitignore

--- build.bat
+++ build.bat
@@ -2,6 +2,8 @@
 :: This is the Windows build script
 :: If you are on Linux, see build.sh
 
+$CoreLibraryDependencies=@("SDL.lib")
+
 pushd build
-cl /Wall ..\main.c
+cl /Wall main.c /link $CoreLibraryDependencies
 popd
